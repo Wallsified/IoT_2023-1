39f2faca625aff84d15841f2d2413368bcf03270
Wallsified <danielparedes@ciencias.unam.mx>
2022-09-17 21:50:05 -0500

    wip p06

--- Clases/C07: Bluetooth/Bluetooth_SPP2/Bluetooth_SPP2.ino
+++ Clases/C07: Bluetooth/Bluetooth_SPP2/Bluetooth_SPP2.ino
@@ -1,18 +1,3 @@
-/* Mini Práctica #6: Bluetooth
-   Objetivos:
-   1. Usando la aplicación Serial Bluetooth Terminal o alguna otra parecida, desarrolle lo siguiente:
-      a. Encender los LED dependiendo el valor enviado, es decir, al enviar un “1” se
-         enciende el LED 1, al enviar “2” se enciende el LED 2 y así sucesivamente,
-         todos se apagan cuando se envíe un “0”
-      b. Hacer que cada vez que se apriete un botón, recibir la temperatura actual en
-         el smartphone y cuando se apriete el otro, la humedad. 
-
-  Código creado por los alumnos
-  - Paredes Zamudio Luis Daniel @Wallsified
-  - González Arceo Carlos Eduardo @Carlos-crea
-
-*/
-
 #include "BluetoothSerial.h"
 #include "DHT.h"
 
@@ -22,20 +7,20 @@
 
 #define LED1 14
 #define LED2 26
-#define DHTPIN 32 //Pin DHT
-#define DHTTYPE 11 //Tipo DHT
+#define DHTPIN 32
+#define DHTTYPE 11
 #define SW1 15
 #define SW2 4
 
-BluetoothSerial BT; //Objeto Bluetooth.
-DHT dht(DHTPIN, DHTTYPE); //Objeto DHT
+BluetoothSerial BT;
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(115200);
-  dht.begin(); //iniciamos el objeto dht
+  dht.begin();
   BT.begin("ESP32_LED_BT"); // Nombre de tu Dispositivo Bluetooth y en modo esclavo
-  Serial.println("Da bluchu dibais is redy to pel"); //yeah
-  BT.register_callback(callback_function); //función callback de BT
+  Serial.println("Da bluchu dibais is redy to pel");
+  BT.register_callback(callback_function);
   pinMode (LED1, OUTPUT);
   pinMode (LED2, OUTPUT);
   pinMode(SW1, INPUT);
@@ -56,7 +41,7 @@ void callback_function(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
     Serial.println("Datos recibidos");
     while (BT.available()) { // Mientras haya datos por recibir
       int incoming = BT.read(); // Lee un byte de los datos recibidos
-      
+
       Serial.print("Recibido: ");
       Serial.println(incoming);
 
@@ -64,7 +49,7 @@ void callback_function(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
         digitalWrite(LED1, HIGH);
         BT.println("LED1 Encendido");
       }
-      else if (incoming == 50) { //2 en ASCII
+      else if (incoming == 50) {
         digitalWrite(LED2, HIGH);
         BT.println("LED2 Encendido");
       }
@@ -80,14 +65,15 @@ void callback_function(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
 
 void loop() {
   delay(2000);
-  float h = dht.readHumidity(); //humidity
-  float f = dht.readTemperature(true); //
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
 
   if (isnan(h) || isnan(t) || isnan(f)) {
-    return; //evitamos cosas de tipo nan en la impresión. 
+    return;
   }
-
-  if (digitalRead(SW2 == HIGH)) { //here's where everything bugs
+  
+  if (digitalRead(SW2 == HIGH)) {
     BT.print("Nivel de Humedad: ");
     BT.println(h);
   }
