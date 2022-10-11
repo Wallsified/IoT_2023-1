f6ea301710a360a978e5aeda63ee7647524e5c68
Wallsified <danielparedes@ciencias.unam.mx>
2022-10-11 17:19:37 -0500

    HTTP works, miss a e s t h e t i c

--- MN08: HTTP/HTTP/HTTP.ino
+++ MN08: HTTP/HTTP/HTTP.ino
@@ -1,24 +1,42 @@
+/* Mini Práctica #8: HTTP
+   Objetivos:
+  Utilizando el ESP32 como servidor, implemente una interfaz de usuario donde monitoree los
+  valores del potenciómetro y la LDR, y que además sea posible modificar el brillo de LED 3
+  con PWM y encender los LEDs 1 y 5.
+
+  Código modificado por los alumnos:
+  - Paredes Zamudio Luis Daniel @Wallsified
+  - González Arceo Carlos Eduardo @Carlos-crea
+
+*/
+
 //Bibliotecas----------------------------------
 #include "WiFi.h"
 #include "ESPAsyncWebServer.h"
 #include "SPIFFS.h"
 
-//Credenciales Red-----------------------------
-const char* ssid = "INFINITUMA6A4_2.4";
-const char* password = "Zamudiov3!";
-const int sensorPin = 34; //LDR
+// Credenciales Red
+const char* ssid = "Mark Wifi";
+const char* password = "achtzehnpfannkuchen18"; //achtzehnpfannkuchen18
 
-//LEDs GPIO-------------------------------------
-#define ledPinBlue 14 //led 1
-#define ledPinRed 33 //led 5
-#define ledThree 26
+// Constantes de la ejecución.
 const char* PARAM_INPUT_1 = "output";
 const char* PARAM_INPUT_2 = "state";
+const char* PARAM_INPUT = "value";
+
+// Constantes para PMW
 const int freq = 5000;
 const int ledChannel = 0;
 const int resolution = 8;
-const char* PARAM_INPUT = "value";
 String sliderValue = "0";
+
+// Pin LDR
+const int sensorPin = 34; 
+
+//LEDs -------------------------------------
+#define ledOne 14 
+#define ledFive 33 
+#define ledThree 26
 
 //Objeto AsyncWebServer, puerto 80-------------
 AsyncWebServer server(80);
@@ -49,22 +67,23 @@ void setup_wifi() {
   });
 }
 
-//Acciones a realizar--------------------------
+// Acciones a realizar durante la ejecución. 
 String processor(const String& var) {
-  //Serial.println(var);
-  if (var == "BUTTONPLACEHOLDER") {
+
+  if (var == "BUTTONPLACEHOLDER") { //switches HTTP
     String buttons = "";
-    buttons += "<h4>LED 1</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"25\" " + outputState(ledPinBlue) + "><span class=\"slider1\"></span></label>";
-    buttons += "<h4>LED 5</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"26\" " + outputState(ledPinRed) + "><span class=\"slider2\"></span></label>";
+    buttons += "<h4>LED 1</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"14\" " + outputState(ledOne) + "><span class=\"slider1\"></span></label>";
+    buttons += "<h4>LED 5</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"33\" " + outputState(ledFive) + "><span class=\"slider2\"></span></label>";
     return buttons;
   }
-  if (var == "SLIDERVALUE") {
+
+  if (var == "SLIDERVALUE") { //slider HTTP
     return sliderValue;
   }
   return String();
 }
 
-//Estado del led-------------------------------
+// Estado de salida del LED
 String outputState(int output) {
   if (digitalRead(output)) {
     return "checked";
@@ -74,6 +93,7 @@ String outputState(int output) {
   }
 }
 
+// Como tratamos el slider del LED + PWM como una salida String, entonces lo leemos de la misma forma
 String ReadSensor() {
   float sensorVal = analogRead(sensorPin) * (3.3 / 4096.0);
   Serial.println(sensorVal);
@@ -83,22 +103,26 @@ String ReadSensor() {
 void setup() {
   Serial.begin(115200);
   setup_wifi();
-  //Leds---------------------------------------
-  pinMode(ledPinBlue, OUTPUT);
-  digitalWrite(ledPinBlue, LOW);
-  pinMode(ledPinRed, OUTPUT);
-  digitalWrite(ledPinRed, LOW);
+
+  // Setup LED's
+  pinMode(ledOne, OUTPUT);
+  digitalWrite(ledOne, LOW);
+  pinMode(ledFive, OUTPUT);
+  digitalWrite(ledFive, LOW);
   pinMode(ledThree, OUTPUT);
   digitalWrite(ledThree, LOW);
 
-  //Activar PWM--------------------------------
+  // Setup PMW
   ledcSetup(ledChannel, freq, resolution);
   ledcAttachPin(ledThree, ledChannel);
   ledcWrite(ledChannel, sliderValue.toInt());
-  
+
+  //Comenzar el servidor-----------------------
+  server.begin();
+
   //Revisar archivos subidos a la memoria flash---
   if (!SPIFFS.begin(true)) {
-    Serial.println("An Error has occurred while mounting SPIFFS");
+    Serial.println("Hubo un error en SPIFFS. Vuelve a enviar los archivos.");
     return;
   }
 
@@ -112,21 +136,16 @@ void setup() {
     request->send(SPIFFS, "/style.css", "text/css");
   });
 
-  //Archivo JS--------------------------------
+  //  Archivo JS--------------------------------
   server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send(SPIFFS, "/script.js", "text/js");
   });
 
-  //Enviar dato sensor-------------------------
-  server.on("/sensor", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send_P(200, "text/plain", ReadSensor().c_str());
-  });
-
-  //Enviar solicitud a <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
+  // WIP- Request entre ESP32 -> Switches HTTP -> ON/OFF LEDs
   server.on("/update", HTTP_GET, [] (AsyncWebServerRequest * request) {
     String inputMessage1;
     String inputMessage2;
-    //Obtener valor de <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
+    // Obtener valor del request de los switches. ON/OFF?>
     if (request->hasParam(PARAM_INPUT_1) && request->hasParam(PARAM_INPUT_2)) {
       inputMessage1 = request->getParam(PARAM_INPUT_1)->value();
       inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
@@ -143,10 +162,15 @@ void setup() {
     request->send(200, "text/plain", "OK");
   });
 
-  //Enviar solicitud a <ESP_IP>/slider?value=<inputMessage>
+  // Enviar información sobre LDR (Voltaje)
+  server.on("/sensor", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send_P(200, "text/plain", ReadSensor().c_str());
+  });
+
+  // Request entre ESP32 -> Slider PWM -> LED 3
   server.on("/slider", HTTP_GET, [] (AsyncWebServerRequest * request) {
     String inputMessage;
-    //Obtener valor de <ESP_IP>/slider?value=<inputMessage>
+    // Obtener valor del slider y convertirlo a intensidad ON.  
     if (request->hasParam(PARAM_INPUT)) {
       inputMessage = request->getParam(PARAM_INPUT)->value();
       sliderValue = inputMessage;
@@ -159,8 +183,6 @@ void setup() {
     request->send(200, "text/plain", "OK");
   });
 
-  //Comenzar el servidor-----------------------
-  server.begin();
 }
 
 void loop() {
--- MN08: HTTP/HTTP/data/script.js
+++ MN08: HTTP/HTTP/data/script.js
@@ -1,12 +1,39 @@
+/* 
+Función que traduce el envio del voltaje del LDR del ESP32 a la interfaz web. 
+*/
 setInterval(function () {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("SENSOR_ID").innerHTML = this.responseText;
-      }
-    };
-    xhttp.open("GET", "/sensor", true);
-    xhttp.send();
-  },
-  100);
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("SENSOR_ID").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/sensor", true);
+  xhttp.send();
+}, 100);
 
+/*  
+Función que hace los cambios entre el valor del slider http y como se ve reflejado en el ESP32
+*/
+
+function updateSliderPWM(element) {
+  var sliderValue = document.getElementById("pwmSlider").value;
+  document.getElementById("textSliderValue").innerHTML = sliderValue;
+  console.log(sliderValue);
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/slider?value=" + sliderValue, true);
+  xhr.send();
+}
+
+/* 
+Función que cambia el estado de los LEDS 1 y 5 del ESP32 según los switches en la interfaz. 
+*/
+function toggleCheckbox(element) {
+  var xhr = new XMLHttpRequest();
+  if (element.checked) {
+    xhr.open("GET", "/update?output=" + element.id + "&state=1", true);
+  } else {
+    xhr.open("GET", "/update?output=" + element.id + "&state=0", true);
+  }
+  xhr.send();
+}
